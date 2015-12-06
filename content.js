94c0d99efd95dfefc6f3ad08fb1d1b3c85eb243f
baran usta <baranusta@sabanciuniv.edu>
2015-12-06 22:16:32 +0200

    bubble is turned to singleton, positioning of bubble fixed

--- content.js
+++ content.js
@@ -1,45 +1,114 @@
-var bubbleDOM = document.createElement('a');
-bubbleDOM.setAttribute('class', 'dictionary-tooltip');
-bubbleDOM.setAttribute('href', '#');
-document.body.appendChild(bubbleDOM);
+var bubble = (function(){
+  var bottom = 0,
+      left = 0,
+      bubbleDom,
+      sentencesContainer,
+      leftColumn,
+      rightColumn;
+      return {
+        createForDom: function(){
+         bubbleDom = document.createElement('div');
+         bubbleDom.setAttribute('class', 'dictionary-bubble');
+
+         wordsContainer = document.createElement('div');
+         wordsContainer.setAttribute('class', 'dictionary-words-container');
+
+
+         leftColumn = document.createElement('div');
+         leftColumn.setAttribute('class', 'dictionary-leftColumn');
+         wordsContainer.appendChild(leftColumn);
+
+         rightColumn = document.createElement('div');
+         rightColumn.setAttribute('class', 'dictionary-rightColumn');
+         wordsContainer.appendChild(rightColumn);
+
+
+         sentencesContainer = document.createElement('div');
+         sentencesContainer.setAttribute('class', 'dictionary-sentences-container');
+
+         bubbleDom.appendChild(wordsContainer);
+         bubbleDom.appendChild(sentencesContainer);
+         document.body.appendChild(bubbleDom);
+       },
+       determineNewPostion: function(){
+         var rect = window.getSelection().getRangeAt(0).getBoundingClientRect();
+         bottom = $(window).height() - rect.top + 4 - window.pageYOffset;
+         left = rect.left + (rect.width/2) - (bubbleDom.offsetWidth/2);
+         if(left<0){
+           left = 0;
+         }
+      },
+       renderPosition: function(){
+         bubbleDom.style.bottom = bottom + 'px';
+         bubbleDom.style.left = left  + 'px';
+       },
+       makeVisible: function(){
+         bubbleDom.style.visibility = 'visible';
+       },
+       makeInvisible: function(){
+         bubbleDom.style.visibility = 'hidden';
+         childrenRemover(leftColumn);
+         childrenRemover(rightColumn);
+         childrenRemover(sentencesContainer);
+       },
+       addToLeftColumn: function(elm){
+         leftColumn.appendChild(elm);
+       },
+       addToRightColumn: function(elm){
+         rightColumn.appendChild(elm);
+       },
+       addToSentenceRow: function(elm){
+         sentencesContainer.appendChild(elm);
+       }
+      };
+}());
+
+var childrenRemover = function(domElmnt){
+  while (domElmnt.firstChild) {
+    domElmnt.removeChild(domElmnt.firstChild);
+  }
+}
+
+var addChildrenFromArray = function(elmntAdder,childrens,truncateLim){
+  for (child of childrens) {
+    var childElm = document.createElement('li');
+    if(child.length > truncateLim){
+      childElm.innerHTML = child.substring(0,truncateLim) + "...";
+    }
+    else {
+      childElm.innerHTML = child;
+    }
+    elmntAdder(childElm);
+  }
+}
 
 var myTranslator = new Translator(0,1,0);
+bubble.createForDom();
 
 // Lets listen to mouseup DOM events.
 document.addEventListener('mouseup', function (e) {
   var selection = window.getSelection().toString();
   if (selection.length > 0) {
-    renderBubble(e.clientX, e.clientY, selection);
+    bubble.makeVisible();
+    bubble.determineNewPostion();
+    bubble.renderPosition();
 
     myTranslator.getMeaningForLeft(selection,3,function(wordArray){
-        for (t of wordArray) {
-          console.log(t);
-        }
+      addChildrenFromArray(bubble.addToLeftColumn,wordArray,15);
+      bubble.renderPosition();
     });
     myTranslator.getMeaningForRight(selection,3,function(wordArray){
-      for (t of wordArray) {
-        console.log(t);
-      }
+      addChildrenFromArray(bubble.addToRightColumn,wordArray,15);
+      bubble.renderPosition();
     });
-    myTranslator.getSentences(selection,3, function(sentenceArray){
-      for (t of sentenceArray) {
-        console.log(t);
-      }
+    myTranslator.getSentences(selection,2, function(sentenceArray){
+      addChildrenFromArray(bubble.addToSentenceRow,sentenceArray,50);
+      bubble.renderPosition();
     });
   }
 }, false);
 
-
-
 // Close the bubble when we click on the screen.
 document.addEventListener('mousedown', function (e) {
-  bubbleDOM.style.visibility = 'hidden';
+  bubble.makeInvisible();
 }, false);
-
-// Move that bubble to the appropriate location.
-function renderBubble(mouseX, mouseY, selection) {
-  bubbleDOM.innerHTML = selection;
-  bubbleDOM.style.top = mouseY + 'px';
-  bubbleDOM.style.left = mouseX + 'px';
-  bubbleDOM.style.visibility = 'visible';
-}
